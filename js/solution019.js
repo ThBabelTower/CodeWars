df53690272a8cf298b92a8adf09d179aef5bd592
Harsh Jariwala <harshjariwala81@gmail.com>
2022-05-26 11:28:18 +0530

    chromosomeCheck

---
+++ js/solution019.js
@@ -0,0 +1,7 @@
+function chromosomeCheck(sperm) {
+    if (sperm ==='XY'){
+      console.log('Congratulations! You\'re going to have a son.')
+    }else if( sperm ==='XX'){
+      console.log('Congratulations! You\'re going to have a daughter.')
+    }
+  }
